1f42cb2d7c95cd60952f101bddb12e0c5564bd11
Mattia Toso <mattia.toso@gmail.com>
2021-06-04 22:31:10 -0700

    Add Playwright and implement scraping

--- raschietto.js
+++ raschietto.js
@@ -1,9 +1,45 @@
 const { Telegraf } = require('telegraf');
+const playwright = require('playwright');
 
-const bot = new Telegraf(process.env.RASCHIETTO_BOT_TOKEN);
+async function main() {
+    const browser = await playwright.chromium.launch({
+        headless: true
+    });
+    
+    const page = await browser.newPage();
+    await page.goto(process.env.URL_TO_SCRAPE);
+    const productsContainerHandle = await page.waitForSelector('.products');
+    const productsHandle = await productsContainerHandle.$$('div[id^="product-"]');
+    const productsStock = [];
+    for await (p of productsHandle) {
+        const productName = await p.$eval('h3', node => node.innerText);
+        let inStock = false;
+        
+        try {
+            inStock = await p.$eval('div.product-price', node => true);
+        } catch (_) {
+            // if not in stock the product-price class is missing throwing an error
+            // but we want to continue 
+        }
 
-bot.telegram.sendMessage(process.env.CHAT_ID, "Hey there!!!");
+        productsStock.push({
+            name: productName,
+            inStock 
+        });
+    }
+    await browser.close();
 
-// Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
+    const bot = new Telegraf(process.env.RASCHIETTO_BOT_TOKEN);
+
+    const msg = productsStock.map(p => {
+        return `${p.name}: ${p.inStock ? '🚨 **In Stock** 🚨' : '~~Not In Stock~~'}\n`;
+    });
+
+    bot.telegram.sendMessage(process.env.CHAT_ID, msg, { parse_mode: 'MarkdownV2' });
+
+    // Enable graceful stop
+    process.once('SIGINT', () => bot.stop('SIGINT'));
+    process.once('SIGTERM', () => bot.stop('SIGTERM'));
+}
+
+main();
