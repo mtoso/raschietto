e9bc60bb32c481e4c42a5ce39b33a037461c0f0a
Mattia Toso <mattia.toso@gmail.com>
2021-06-07 19:01:06 -0700

    Handle change in the stock of products using checksum file

--- raschietto.js
+++ raschietto.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
 const { Telegraf } = require('telegraf');
 const playwright = require('playwright');
+const crypto = require('crypto');
+const { readFileSync, writeFileSync, existsSync } = require('fs');
 
 const urlToScrape = process.env.URL_TO_SCRAPE;
 const raschiettoBotToken = process.env.RASCHIETTO_BOT_TOKEN;
 const chatId = process.env.CHAT_ID;
+const stockOfProductsChecksumFile = 'stocksOfProductsChecksum.txt';
 
 async function main() {
     const browser = await playwright.chromium.launch({
@@ -15,32 +18,51 @@ async function main() {
     await page.goto(urlToScrape);
     const productsContainerHandle = await page.waitForSelector('.products');
     const productsHandle = await productsContainerHandle.$$('div[id^="product-"]');
-    const productsStock = [];
+    const products = [];
     for await (p of productsHandle) {
         const productName = await p.$eval('h3', node => node.innerText);
-        let inStock = false;
-        
+        const productPath = await p.$eval('a', node => node.getAttribute('href'));
+        let productPrice = undefined;
         try {
-            inStock = await p.$eval('div.product-price', node => true);
+            productPrice = await p.$eval('div.product-price > h3', node => node.innerText);
         } catch (_) {
             // if not in stock the product-price class is missing throwing an error
             // but we want to continue 
         }
-
-        productsStock.push({
+        products.push({
             name: productName,
-            inStock 
+            path: productPath,
+            price: productPrice 
         });
     }
     await browser.close();
 
     const bot = new Telegraf(raschiettoBotToken);
-
-    const msg = productsStock.map(p => {
-        return `${p.name}: ${p.inStock ? '🚨 <strong>In Stock</strong> 🚨' : '<del>Not In Stock</del>'}`;
+    const baseUrl = new URL(urlToScrape).origin;
+    const msg = products.map(p => {
+        return `${p.price ? `🚨 <a href="${new URL(p.path, baseUrl)}">${p.name} - ${p.price}</a> 🚨` : `<del>${p.name}</del>`}`;
     }).join('\n');
 
-    bot.telegram.sendMessage(chatId, msg, { parse_mode: 'HTML' });
+    const currentStockOfProductsMd5 = crypto.createHash('md5').update(msg).digest("hex");
+
+    if (!existsSync(stockOfProductsChecksumFile)) {
+        console.info('Checksum file does not exists, sending first update');
+        // write to the file
+        writeFileSync(stockOfProductsChecksumFile, currentStockOfProductsMd5);
+        // send the msgs
+        bot.telegram.sendMessage(chatId, msg, { parse_mode: 'HTML', disable_web_page_preview: true });
+    } else {
+        // read the prev value of file
+        const pervStockOfProductsMd5 = readFileSync(stockOfProductsChecksumFile).toString();
+        // compare the md5
+        if (currentStockOfProductsMd5 != pervStockOfProductsMd5) {
+            console.info('Checksum changed, sending update');
+            writeFileSync(stockOfProductsChecksumFile, currentStockOfProductsMd5);
+            bot.telegram.sendMessage(chatId, msg, { parse_mode: 'HTML', disable_web_page_preview: true });
+        } else {
+            console.info('Checksum did not change');
+        }
+    }
 
     // Enable graceful stop
     process.once('SIGINT', () => bot.stop('SIGINT'));
